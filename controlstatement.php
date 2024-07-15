83878a293d8ea5ff5063a53c14bc7c9e6805eb29
matsaina <matsaina@gmail.com>
2024-07-15 14:32:19 +0300

    second commit

--- controlstatement.php
+++ controlstatement.php
@@ -1,8 +1,6 @@
 <?php
-
+//   if else statements 
 $num = 9;
-
-
 if ($num%2==0){
 
     echo "even number";
@@ -11,7 +9,6 @@ if ($num%2==0){
     echo "odd number";
 echo "<br>";
 }
-
 $age = 19;
 if($age >= 18 ){
 
@@ -23,17 +20,18 @@ if($age >= 18 ){
 }
 echo "<br>";
 
+
+// while loop 
 $counter = 0;
-
 while ($counter <= 100){
-
     echo $counter;
     $counter +=5;
 }
 echo "<br>";
 
-$students= array("john", "james","sharon","jane");
 
+// for each loop 
+$students= array("john", "james","sharon","jane");
 foreach ($students as $student){
 
     echo $student;
@@ -42,7 +40,27 @@ echo "<br>";
 }
 
 
+// for loop 
+for($i = 10; $i>=0; $i--)
+{
+echo $i;
+echo "<br>";
 
+}
 
+//   do while loop 
+$b = 0;
+do {
+echo $b;
+    $b++;
+echo "<br>";
 
+}while($b < 6);
 
+// Add numbers in an array 
+$sum = 0;
+$numarrs = array(10,20,5,5,20,10);
+foreach ($numarrs as $arr){
+$sum  = $sum + $arr;
+}
+echo "total sum-".$sum;
